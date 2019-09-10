4a578cc28c2ab50a4d39437f556cd0289c88e2e5
pamcuellas <pamcuellas@gmail.com>
2019-09-10 14:51:38 -0400

    Search process finished.

--- static/js/app.js
+++ static/js/app.js
@@ -1,40 +1,53 @@
 /*jshint esversion: 6 */
 
-// from data.js
-const tableData = data;
-const keysArray = ['datetime','country','state','city','shape'];
 
-const filters = [
-	{filter: "datetime"},
-	{filter: "country"},
-	{filter: "state"},
-	{filter: "city"},
-	{filter: "shape"}
-];
+let capitalize_Words = str => {
+	return str.replace(/\w\S*/g, (word) => {
+							return word.charAt(0).toUpperCase() + word.substr(1).toLowerCase();
+						});
+};
 
-let currFilters = [];
+let formatDate = dateStr => {
+	let date = dateStr.split("/").map( num => (num.length === 1 ? '0'+ num : num ) );
+	return (date[2] + '-' +  date[1] + '-' + date[0]);  
+};
 
+// Change the fields position and format.
+const tableData = data.map( obj => {
+					const newStruct = {
+										date: formatDate(obj.datetime),
+										country: obj.country.toUpperCase(),
+										state: obj.state.toUpperCase(),
+										city: capitalize_Words(obj.city),
+										shape: capitalize_Words(obj.shape),
+										durationMinutes: obj.durationMinutes,
+										comments: obj.comments
+									};
+					return newStruct;
+				});
 
 let filteredData = tableData;
-
+const keysArray = ['date','country','state','city','shape'];
+let currFilters = [];
 
 d3.select("#reset-filter").on('click', () => {
 	d3.select("tbody").remove();
 	filteredData = tableData;
-
 });
-
-
 
 // Generic change event for all select elements (or combo-boxes)
 d3.selectAll('select').on('change', () => {
 
+	// Grab info of the changed combo-box.
+	var currIDchanged = d3.select(d3.event.target).attr("id");	
 
 	// Get all selected filters and store in an array of objects.
 	currFilters = keysArray.reduce( (acc, key) => {
 		// Get the value for current filter
-		const value = d3.select('#'+ key).node().value;
-		// Just return if it already has a value.
+		let  value = d3.select('#'+ key).node().value;
+
+		// Just return if it already has a value (Remark: I am using the select element name at ).
+		// if (!keysArray.includes( value )) acc.push( { [key]: value});
 		if (value != "") acc.push( { [key]: value});
 		return acc;
 	}, []);
@@ -55,174 +68,116 @@ d3.selectAll('select').on('change', () => {
 			const keyValueData = dataObj[keyNameFilter];
 
 			// returns if the current data field match the correspondent filter
-			return ( keyValueData.toUpperCase() === keyValueFilter.toUpperCase() );
+			return ( keyValueData === keyValueFilter );
 		});
 	
 	});	
 
 	console.log(filteredData);
 
+	updComboBoxes(currIDchanged);
 
-	// // Grab info of the changed combo-box.
-	// var currIDchanged = d3.select(d3.event.target).attr("id");
-	// var currValue = d3.select('#'+currIDchanged).node().value;
-
-	// // Check if any other combo-box has already a value... 
-	// const isSelectedFilter = keysArray.some( (combo) => { 
-	// 	return ( (d3.select('#'+combo).node().value != "") && (currIDchanged != combo) ); 
-	// });
-	// // ... if not we will use the complete data source (tableData).
-	// if (!isSelectedFilter) {
-	// 	filteredData = tableData;
-	// }
-
-	// // Just update stuff if changed for a value different from empty ("")
-	// if (currValue != "") {
-
-	// 	/* Filter/Update data based on the last combo-box changed */ 
-	// 	filteredData = filteredData.filter( obj => (currValue.toUpperCase() === obj[currIDchanged].toUpperCase()) );	
-	
-	// 	// Update all combo-boxes different from the current one. 
-	// 	updComboBoxes( currIDchanged, currValue );
-	
-	// 	// Grab the table  
-	// 	let table = d3.select("#ufo-table");
-
-	// 	// Remove any previous table selection  
-	// 	d3.select("tbody").remove();	
-	
-	// 	// Create the tbody with the new data selection/filter
-	// 	table.append("tbody")
-	// 		.selectAll("tr")
-	// 			.data( filteredData )
-	// 			.enter()
-	// 			.append("tr")
-	// 		.selectAll("td")
-	// 			.data(  obj => d3.values(obj) )
-	// 			.enter()
-	// 			.append("td")
-	// 			.text( d => d );
-	// }
+	updateTable();
 
 });
 
+let updateTable = () => { 
 
-/* update all combo-boxes but the changed one */ 
-let updComboBoxes = ( id, value ) => {
+	// Grab the table  
+	let table = d3.select("#ufo-table");
 
-	keysArray.forEach( key => {
+	// Remove any previous table selection  
+	d3.select("tbody").remove();	
 
-		// Grab the current Key value
-		let currKeyValue = d3.select("#" + key).node().value;
-
-		// Update combo-box only if it is different from changed combo-box (id) and if the current key does not already have a value.
-		if ( (key != id) && (currKeyValue === "") ) {
-
-			console.log("Updating ", key, id);
-
-			/* Clear the current key (or combo-box) */
-			// d3.select("#" + key).text("");
-
-			/* Filter key according to the parameter id */
-			let currKey = filteredData.reduce( (acc, obj) => {
-				if (obj[id].toUpperCase() === value.toUpperCase() ) {
-					acc.push(obj[key]);
-				}
-				return acc;
-			},[" "]);
-		
-			console.log(currKey);
-
-
-
-			// Select the combo-box 
-			let comboID = d3.select("#" + key);
-
-			// Clear the combo-box 
-			comboID.text("");
-
-
-
-
-			// Update combo box according to the current key/id 
-			currKey = [...new Set(currKey)];
-			currKey.sort();
-			comboID
-				.selectAll("option")  
-				.data(currKey)  
-				.enter()
-				.append("option")  
-					.text( d => d) 
-				.classed("capitalize",true);
-
-		}
-	});
+	// Create the tbody with the new data selection/filter
+	table.append("tbody")
+		.selectAll("tr")
+			.data( filteredData )
+			.enter()
+			.append("tr")
+		.selectAll("td")
+			.data(  obj => d3.values(obj) )
+			.enter()
+			.append("td")
+			.text( d => $('<div>').html(d).text() )
+			.classed('align-middle', true);
+			 
 };
 
 
 
+/* update all combo-boxes based on the current filters */ 
+let updComboBoxes = ( id ) => {
+
+	keysArray.forEach( (key, index) => {
+
+		// Get the selected value of current combo.
+		const currOptSelected = d3.select("#" + key).node().value; 
+
+		// Update all combo-boxes but not the changed one.
+		if (key != id)  {
+
+			/* Grab data for current combo-box */
+			let currKeyData = filteredData.reduce(  (acc , obj) =>  { 
+				acc.push(obj[key]); 
+				return acc; 
+			},[" "]);
+
+			// Remove duplicated values
+			currKeyData = [...new Set(currKeyData)];
+			// Sort the values
+			currKeyData.sort();
 
 
+			// Select current combo-box 
+			let comboID = d3.select("#" + key);
 
-/*****************************  Populate Date ******************************/ 
-let datetime = tableData.map( obj => obj.datetime);
-datetime = [...new Set(datetime)];
-datetime.sort();
-d3.select("#datetime")
-	.selectAll("option")  
-	.data(datetime)  
-	.enter()   
-	.append("option")  
-		.text( d => d);
+			// Clear current combo-box content 
+			comboID.text("");
+
+			// Update data combo-box according to the current key/id 
+			comboID
+				.selectAll("option")
+				.data(currKeyData)
+				.enter()
+				.append("option")  
+					.text( d => d);
+		}
+
+		// Return the selected value if it already has one.
+		if ( currOptSelected ) {
+			d3.select("#" + key).property("value", currOptSelected);
+		}
+	});
+};
+
+// Initialize combo-boxes.
+updComboBoxes ( " " );
 
 
-
-
-
-/*****************************  Populate countries ******************************/ 
-let countries = tableData.map( obj => obj.country.toUpperCase());
-countries = [...new Set(countries)];
-countries.sort();
-d3.select("#country")
-	.selectAll("option")  
-	.data(countries)  
-	.enter()   
-	.append("option")  
-		.text( d => d);
-
-/*****************************  Populate states ******************************/ 
-let states = tableData.map( obj => obj.state.toUpperCase());
-states = [...new Set(states)];
-states.sort();
-d3.select("#state")
-	.selectAll("option")  
-	.data(states)  
-	.enter()   
-	.append("option")  
-		.text( d => d);
-
-/*****************************  Populate cities ******************************/ 
-let cities = tableData.map( obj => obj.city);
-cities = [...new Set(cities)];
-cities.sort();
-d3.select("#city")
-	.selectAll("option")  
-	.data(cities)  
-	.enter()   
-	.append("option")  
-		.text( d => d);
- 
-
-/*****************************  Populate Duration Minutes ******************************/ 
-let shapes = tableData.map( obj => obj.shape);
-shapes = [...new Set(shapes)];
-shapes.sort();
-// console.log(shapes);
-d3.select("#shape")
-	.selectAll("option")  
-	.data(shapes)  
-	.enter()   
-	.append("option")  
-		.text( d => d);
- 
-
+$("select").on("click" , function() {
+  
+	$(this).parent(".select-box").toggleClass("open");
+	
+  });
+  
+  $(document).mouseup(function (e)
+  {
+	  var container = $(".select-box");
+  
+	  if (container.has(e.target).length === 0)
+	  {
+		  container.removeClass("open");
+	  }
+  });
+  
+  
+  $("select").on("change" , function() {
+	
+	var selection = $(this).find("option:selected").text(),
+		labelFor = $(this).attr("id"),
+		label = $("[for='" + labelFor + "']");
+	  
+	label.find(".label-desc").html(selection);
+	  
+  });
